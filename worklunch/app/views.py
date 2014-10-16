b254887972e0f160e257bdd02394dfbe0f1089a2
Michael Woods <physicsmichael@gmail.com>
2014-10-16 08:39:01 -0700

    Improved js and python communication.

--- worklunch/app/views.py
+++ worklunch/app/views.py
@@ -3,6 +3,7 @@ from flask import render_template, request, jsonify
 from app import app
 import sqlite3
 import json
+import os
 
 # ROUTING/VIEW FUNCTIONS
 @app.route('/')
@@ -20,6 +21,25 @@ def _read_db():
 @app.route('/_add_to_db', methods=['GET'])
 def _add_to_db():
   data = request.args.get('data')
+  db = request.args.get('db')
+  if not db.endswith('.db'):
+    db += '.db'
+  # The default database shouldn't save markers added.
+  if data=='default':
+    pass
+  #import pdb; pdb.set_trace();
+  # TODO Create a response if the db doesn't exist to inform user.
+  db_path = "app/static/" + db
+
+  #if not os.path.exists("db_path"):
+    #return
+  conn = sqlite3.connect(db_path)
+  c = conn.cursor()
+  c.execute("CREATE TABLE geolocs (loc text)")
+  print data, repr(data)
+  c.execute("INSERT INTO geolocs VALUES (?)", (data,))
+  conn.commit()
+
   print "Request Args:", request.args
   print data, type(data)
   print jsonify(result=data)
